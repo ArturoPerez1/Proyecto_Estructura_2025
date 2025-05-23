f41f8d94ba953f231a620a9f23389f9152204002
Arturo Pérez <arjperez31@gmail.com>
2025-05-23 15:46:27 -0400

    añadiendo la fución createPlayerList a player.h

--- Librerias/player.h
+++ Librerias/player.h
@@ -1,16 +1,20 @@
 #include <string>
+#include <iostream>
 #include "./cardDeck.h"
 
-struct Node_Card{
+struct Node_Card
+{
     Card card;
-    Node_Card *next_card; 
+    Node_Card *next_card;
 };
 
 /*
-    -Importante: Los jugadores estarán almacenados dentro de una lista enlazada. Por lo que 
-    cada jugador será un nodo de la lista y esos nodos de la lista tienen la siguiente 
+    Importante:
+    Los jugadores estarán almacenados dentro de una lista enlazada. Por lo que
+    cada jugador será un nodo de la lista y esos nodos de la lista tienen la siguiente
     información:
 
+    Atributos:
     - id_player: identificador del jugador (1 ó 2 ó 3 ó 4)
     - score_player: es la puntuación acumulada
     - name_player: nombre del jugardor (ejem: "Arturo")
@@ -20,15 +24,40 @@ struct Node_Card{
 
 struct Node_Player
 {
-    int id_player;
-    int score_player;
-    std::string name_player;
-    std::string position_player;
+    int id_player = 0;
+    int score_player = 0;
+    std::string name_player = "";
+    std::string position_player = "";
     Node_Card *deck_player;
     Node_Player *next_player;
 };
 
+void createDeckPlayer(Node_Card *&deck_player) {
 
+};
 
+void createPlayerList(Node_Player *&list_players, std::string name_player, int id)
+{
+    Node_Player *new_player = new Node_Player();
+    Node_Player *aux1 = list_players;
+    Node_Player *aux2;
 
+    new_player->name_player = name_player;
 
+    while (aux1 != NULL)
+    {
+        aux2 = aux1;
+        aux1 = aux1->next_player;
+    }
+
+    if (aux1 == list_players)
+    {
+        list_players = new_player;
+    }
+    else
+    {
+        aux2->next_player = new_player;
+    }
+
+    new_player->next_player = aux1;
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <iostream>
+#include "./Librerias/player.h"
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-
-    cout << "Hola mundo que onda" << endl;
+    cout << "Hola mundo" << endl;
     return 0;
 }
